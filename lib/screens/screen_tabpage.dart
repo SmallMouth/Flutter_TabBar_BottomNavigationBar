27c4f6ec135c6fa51d6b535af2c6f36786638fa2
SmallMouth <lismallmouth@gmail.com>
2019-02-26 15:46:33 +0800

    add NestedScrollView/SliverAppBar

--- lib/screens/screen_tabpage.dart
+++ lib/screens/screen_tabpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bar/screens/screen_listpage.dart';
 
 class TabPage extends StatefulWidget {
   final String contant;
@@ -12,26 +13,40 @@ class TabPage extends StatefulWidget {
   }
 }
 
-class TabPageState extends State<TabPage> {
+class TabPageState extends State<TabPage> with SingleTickerProviderStateMixin {
   final String contant;
+
+  TabController _tabController;
+  ScrollController _scrollViewController;
 
   TabPageState(this.contant);
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(vsync: this, length: 3);
+    _scrollViewController = ScrollController(initialScrollOffset: 0.0);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _scrollViewController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final _tabOptions = [
-      Column(
-        children: <Widget>[
-          Text(contant),
-          Text('Child 0'),
-        ],
-      ),
+      ListPage(),
       Column(
         children: <Widget>[
           Text(contant),
           Text('Child 1'),
         ],
-      ),    Column(
+      ),
+      Column(
         children: <Widget>[
           Text(contant),
           Text('Child 2'),
@@ -39,21 +54,47 @@ class TabPageState extends State<TabPage> {
       ),
     ];
 
-    return DefaultTabController(
-        length: _tabOptions.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Container(
-              child: TabBar(
-                tabs: [
-                  Tab(text: 'Tab0'),
-                  Tab(text: 'Tab1'),
-                  Tab(text: 'Tab2'),
-                ],
-              ),
-            ),
-          ),
-          body: TabBarView(children: _tabOptions),
-        ));
+    return Scaffold(
+      body: NestedScrollView(
+          headerSliverBuilder: (context, boxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                title: Text('Tab Controller Example'),
+                pinned: true,
+                floating: true,
+                forceElevated: boxIsScrolled,
+                expandedHeight: 200,
+                bottom: TabBar(
+                  tabs: <Widget>[
+                    Tab(text: 'Tab0'),
+                    Tab(text: 'Tab1'),
+                    Tab(text: 'Tab2'),
+                  ],
+                  controller: _tabController,
+                ),
+              )
+            ];
+          },
+          body: TabBarView(
+            children: _tabOptions,
+            controller: _tabController,
+          )),
+    );
+//    return DefaultTabController(
+//        length: _tabOptions.length,
+//        child: Scaffold(
+//          appBar: AppBar(
+//            title: Container(
+//              child: TabBar(
+//                tabs: [
+//                  Tab(text: 'Tab0'),
+//                  Tab(text: 'Tab1'),
+//                  Tab(text: 'Tab2'),
+//                ],
+//              ),
+//            ),
+//          ),
+//          body: TabBarView(children: _tabOptions),
+//        ));
   }
 }
